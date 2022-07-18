da34db857ed606960dfaface98c7287e13230a1e
Agrawal Ayush <ayush.agrawal@dell.com>
2022-07-18 21:48:45 +0530

    initial commit

---
+++ frontend/app/pages/index.jsx
@@ -0,0 +1,11 @@
+import Page from '../layouts/page';
+
+const Index = () => (
+  <Page>
+    <div className="index">
+      <h1>Blanka</h1>
+    </div>
+  </Page>
+);
+
+export default Index;
---
+++ server/README.md
@@ -0,0 +1,1 @@
+# this is the wesite template for guild website
